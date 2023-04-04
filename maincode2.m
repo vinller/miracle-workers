064777e283f32f3ec5f0c31293f24efee9cbbc0f
joshgsarb <126526442+joshgsarb@users.noreply.github.com>
2023-04-04 12:36:46 -0700

    Update maincode2.m

--- maincode2.m
+++ maincode2.m
@@ -2,7 +2,7 @@ clearvars
 brick = ConnectBrick("gripy");
 global key
 InitKeyboard();
-SPEED = -40;
+SPEED = 40;
 LMOTOR = 'A';
 RMOTOR = 'B';
 DMOTOR = 'D';
@@ -33,4 +33,53 @@ while 1 %infinite loop
                 brick.ResetMotorAngle(LMOTOR); 
                 brick.ResetMotorAngle(RMOTOR); 
                 brick.MoveMotorAngleRel(LMOTOR, SPEED, TURNING_RADIUS, 'Coast');
-                
+            elseif distSense 
+            
+            end
+        
+        case 5
+            disp('Stop for 3 seconds');
+            brick.StopAllMotors();
+            pause(3.0);
+            brick.MoveMotor(LMOTOR, SPEED);
+            brick.MoveMotor(RMOTOR, SPEED);
+        otherwise
+            disp('manual mode');
+            brick.StopAllMotors();
+            switch key
+                case 'uparrow'         %For moving forward
+                     disp('up Arrow Pressed!');
+                     brick.MoveMotor(RMOTOR, SPEED)
+                     brick.MoveMotor(LMOTOR, SPEED)
+            
+                 case 'downarrow'           %For moving backward
+                     disp('down Arrow Pressed!');
+                     brick.MoveMotor(RMOTOR, -SPEED)
+                     brick.MoveMotor(LMOTOR, -SPEED)
+            
+                 case 'leftarrow'        %For turning left
+                     disp('Left Arrow Pressed!');
+                     brick.MoveMotor(RMOTOR, SPEED)
+                     brick.MoveMotor(LMOTOR, -SPEED)
+            
+                 case 'rightarrow'         %For turning right
+                     disp('Right Arrow Pressed!');
+                     brick.MoveMotor(RMOTOR,-SPEED)
+                     brick.MoveMotor(LMOTOR,SPEED)
+            
+                 case 'w'                 %Raises the lift
+                     disp('w pressed')
+                     brick.MoveMotor(FMOTOR,15)
+            
+                 case 's'                 %Lowes the lift
+                     disp('s pressed')
+                     brick.MoveMotor(FMOTOR,-15)
+            
+                 case 0                   %No key is being pressed.
+                     disp('No Key Pressed');
+                     brick.StopMotor('A')
+                     brick.StopMotor('D')
+                     brick.StopMotor('B')
+        end
+    end
+end
