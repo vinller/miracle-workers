5463fd0824c18f9b6be4a90bdcd9c7586c09b03e
joshgsarb <126526442+joshgsarb@users.noreply.github.com>
2023-04-03 19:01:47 -0700

    Update maincode2.m

--- maincode2.m
+++ maincode2.m
@@ -9,8 +9,16 @@ DMOTOR = 'D';
 colorPort = 4;
 distPort = 3;
 buttonPort = 2;
+turningRadius = 430
 
 while 1 %infinite loop
     colorSense = brick.ColorCode(colorSensePort); %turn on color sensor
     distSense = brick.UltrasonicDist(distSensePort); %turn on ultrasonic sensor
     buttonSense = brick.TouchPressed(buttonSensePort); %turn on touch sensor
+    switch colorSense
+        case 1
+            if distSense < 40 && buttonSense == 0
+                brick.MoveMotor(LMOTOR, SPEED);
+                brick.MoveMotor(RMOTOR, SPEED);
+            elseif buttonSense == 1
+                
